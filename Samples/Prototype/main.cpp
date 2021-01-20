09f7053d60e5b0ecd1e72f966190a5c7b5c015f4
niiode <niiode.deepr@gmail.com>
2021-01-20 12:13:05 +0100

    [Add] CMake config

---
+++ Samples/Prototype/main.cpp
@@ -0,0 +1,20 @@
+// Copyright 2021 Niiode. All Rights Reserved.
+
+#include <string>
+#include <iostream>
+
+#define LOG(_str) std::cout << _str << std::endl;
+
+
+int main()
+{
+	LOG("=== Start ===\n");
+
+
+	
+
+
+	LOG("\n=== End ===");
+
+	return 0;
+}
